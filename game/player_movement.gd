2baf0f61d961379c270fba955a2dcac31e0df1b8
Marco Túlio <marcotmotta@gmail.com>
2020-02-20 17:46:19 -0300

    background and player flip

--- game/player_movement.gd
+++ game/player_movement.gd
@@ -1,21 +1,37 @@
 extends KinematicBody2D
 
-export (int) var speed = 300
+export (int) var run_speed = 300
+export (int) var jump_speed = -700
+export (int) var gravity = 1500
 
 var velocity = Vector2()
+var floor_normal = Vector2(0, -1)
+var jumping = false
 
 func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed('right'):
-		velocity.x += 1
-	if Input.is_action_pressed('left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('down'):
-		velocity.y += 1
-	if Input.is_action_pressed('up'):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
+	velocity.x = 0
+	var right = Input.is_action_pressed('right')
+	var left = Input.is_action_pressed('left')
+	var jump = Input.is_action_just_pressed('up')
+
+	if jump and is_on_floor():
+		jumping = true
+		velocity.y = jump_speed
+	if right:
+		velocity.x += run_speed
+		$AnimatedSprite.set_flip_h(false)
+	if left:
+		velocity.x -= run_speed
+		$AnimatedSprite.set_flip_h(true)
+
+	if velocity.x != 0:
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
 
 func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity)
+	velocity.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
+	velocity = move_and_slide(velocity, floor_normal)
