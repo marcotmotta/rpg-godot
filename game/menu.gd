26ff02bb3fde27669397823d8463d7de9cf639c9
Marco Túlio <marcotmotta@gmail.com>
2020-02-26 13:10:20 -0300

    start sfx

--- game/menu.gd
+++ game/menu.gd
@@ -9,10 +9,8 @@ func _on_Play_gui_input(event):
 	and event.is_pressed():
 		get_tree().change_scene("res://Scene1.tscn")
 
-
 func _on_MenuSong_finished():
 	$MenuSong.play();
-
 
 func _on_Quit_gui_input(event):
 	if event is InputEventMouseButton \
