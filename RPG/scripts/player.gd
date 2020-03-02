c0199b1be20f5a330e2907657781717039312ec8
Gustavo Zille <guzillaine@gmail.com>
2020-03-01 21:02:42 -0300

    Formatting Player Script.

--- RPG/scripts/player.gd
+++ RPG/scripts/player.gd
@@ -1,93 +1,104 @@
 extends KinematicBody2D
 
-# camera signal
+# Camera signal.
 signal grounded_updated(is_grounded)
-
-enum {
-	IDLE,
-	MOVE,
-	ATTACK,
-	HURT,
-	DEAD
-}
 
 export (int) var run_speed = 200
 export (int) var jump_speed = -550
+export (int) var health = 500
 export (int) var damage = 40
 
 onready var global = get_node("/root/global")
 
-var velocity = Vector2()
 var jumping = false
+var blocking = false
 var attacking = false
+var taking_damage = false
+var velocity = Vector2()
 
-# camera variables
+# Camera variables.
 var is_grounded
 var facing = 0
 
 func _ready():
 	$AnimatedSprite.play("idle")
 
-func get_input():
-	velocity.x = 0
-	facing = 0
-	var right = Input.is_action_pressed('right')
+func _input_process():
 	var left = Input.is_action_pressed('left')
+	var right = Input.is_action_pressed('right')
+	var block = Input.is_action_pressed("down")
 	var jump = Input.is_action_just_pressed('up')
 	var attack = Input.is_action_just_pressed('attack')
-
-	if !attacking:
+	
+	if health > 0 and !attacking and !taking_damage:
+		facing = 1 if right else -1 if left else 0
+		
 		if attack:
-			attack_start()
+			attacking = true
+			$AnimatedSprite.play("attack_step_01")
+			
 		else:
-			if jump and is_on_floor():
-				jumping = true
-				velocity.y = jump_speed
-			if right:
-				facing += 1
-				velocity.x += run_speed
-				$AnimatedSprite.set_flip_h(false)
-				$AttackRange/CollisionShape2D.position.x = abs($AttackRange/CollisionShape2D.position.x)
-			if left:
-				facing -= 1
-				velocity.x -= run_speed
-				$AnimatedSprite.set_flip_h(true)
-				$AttackRange/CollisionShape2D.position.x = -abs($AttackRange/CollisionShape2D.position.x)
-
-			if velocity.x != 0:
-				$AnimatedSprite.play("run")
+			if block:
+				$AnimatedSprite.play("block")
+				blocking  = true
+			
 			else:
-				$AnimatedSprite.play("idle")
+				if jump and is_on_floor():
+					velocity.y = jump_speed
+					jumping = true
+				
+				if right or left:
+					velocity.x += facing * run_speed
+					$AnimatedSprite.set_flip_h(facing < 0)
+					$AttackRange/CollisionShape2D.position.x = facing * abs($AttackRange/CollisionShape2D.position.x)
+					$AnimatedSprite.play("run")
+				
+				else:
+					$AnimatedSprite.play("idle")
 
 func _physics_process(delta):
-	get_input()
-	velocity.y += global.gravity * delta
-	
-	velocity = move_and_slide(velocity, global.floor_normal)
-
-	if jumping and is_on_floor():
-		jumping = false
-
-	# Camera configs
+	# Camera configs.
 	var was_grounded = is_grounded
 	is_grounded = is_on_floor()
+	
+	velocity.x = 0
+	velocity.y += global.gravity * delta
+	
+	_input_process()
+	
+	velocity = move_and_slide(velocity, global.floor_normal)
+	
+	if jumping and is_on_floor():
+		jumping = false
 	
 	if was_grounded == null || is_grounded != was_grounded:
 		emit_signal("grounded_updated", is_grounded)
 
-func attack_start():
-	attacking = true
-	$AnimatedSprite.play("attack1")
+func take_damage(damage):
+	if !blocking:
+		health -= damage
+		
+		if health > 0:
+			$AnimatedSprite.play("hurt")
+			taking_damage = true
+		
+		else:
+			$AnimatedSprite.play("death")
 
 func _on_AnimatedSprite_animation_finished():
-	if $AnimatedSprite.animation == 'attack1':
-		$AnimatedSprite.play("attack2")
-		$Attack.play()
+	if $AnimatedSprite.animation == 'attack_step_01':
 		$AttackRange/CollisionShape2D.disabled = false
-	elif $AnimatedSprite.animation == 'attack2':
-		attacking = false
+		$AnimatedSprite.play("attack_step_02")
+		$AttackSound.play()
+		
+	elif $AnimatedSprite.animation == 'attack_step_02':
 		$AttackRange/CollisionShape2D.disabled = true
 		$AnimatedSprite.play("idle")
+		attacking = false
+	
+	elif $AnimatedSprite.animation == 'hurt':
+		$AnimatedSprite.play("idle")
+		taking_damage = false
 
 func _on_AttackRange_body_entered(body):
 	if(body.is_in_group('Enemies')):
