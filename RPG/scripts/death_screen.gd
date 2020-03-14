260f3d3b5c854a3f447509fef740b7b32ce1d8fe
Marco Túlio <marcotmotta@gmail.com>
2020-03-13 22:48:02 -0300

    better death screen

--- RPG/scripts/death_screen.gd
+++ RPG/scripts/death_screen.gd
@@ -1,4 +1,20 @@
 extends CanvasLayer
 
-func _on_Timer_timeout():
-	get_tree().reload_current_scene()
+func _ready():
+	$ScreenAnimation.play("fade")
+
+func _on_ScreenAnimation_animation_finished(anim_name):
+	if anim_name == "fade":
+		$Particles2D.visible = true
+		$LabelAnimation.play("label")
+		$VBoxContainer/Retry.visible = true
+		$VBoxContainer/Menu.visible = true
+
+func _on_Retry_gui_input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		get_tree().reload_current_scene()
+
+func _on_Menu_gui_input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		#enquanto n tem menu, esse botao quita
+		get_tree().quit()
